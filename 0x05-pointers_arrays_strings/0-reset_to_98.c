50ed828295bc8d343741ec4903b27bf97a173deb
elsayed-elsaghir <elsayedelsaghir303@gmail.com>
2023-07-25 19:13:18 -0700

    task#0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* void reset_to_98 - resets a value of an integer
+*
+* @n: value of dereferance pointer
+*
+* Return: nth
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
